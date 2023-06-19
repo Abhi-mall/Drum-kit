80c51a0da884f9a03970d37f3ce6379abe8a586f
Abhishek mall <122859957+Abhi-mall@users.noreply.github.com>
2023-06-19 05:47:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,107 @@
+var nuberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for(var i=0; i<nuberOfDrumButtons; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        
+        var buttonInnerHTML=this.innerHTML;
+
+        makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
+
+    });
+}
+
+document.addEventListener("keypress", function(event){
+
+
+    makeSound(event.key)
+    buttonAnimation(event.key);
+});
+
+
+
+
+
+
+
+
+
+
+      function makeSound(key){
+
+
+        switch (key) {
+           
+
+                case "w":
+
+                var audio=new Audio('sound/tom-1.mp3');
+                audio.play();
+
+                break;
+
+                case "a":
+
+                var audio=new Audio('sound/tom-2.mp3');
+                audio.play();
+
+                break;
+
+                case "s":
+
+                var audio=new Audio('sound/tom-3.mp3');
+                audio.play();
+
+                break;
+
+                case "d":
+
+                var audio=new Audio('sound/tom-4.mp3');
+                audio.play();
+
+                break;
+
+                case "j":
+
+                var audio=new Audio('sound/crash.mp3');
+                audio.play();
+
+                break;
+
+                case "k":
+
+                var audio=new Audio('sound/kick-bass.mp3');
+                audio.play();
+
+                break;
+
+                case "l":
+
+                var audio=new Audio('sound/snare.mp3');
+                audio.play();
+
+                break;
+            
+        
+            default:console.log(buttonInnerHTML);
+               
+        }
+   
+}
+
+
+function buttonAnimation(currentkey){
+
+    var activeButtton= document.querySelector("."+currentkey);
+    
+    activeButtton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButtton.classList.remove("pressed"),100
+    });
+
+
+}
+
